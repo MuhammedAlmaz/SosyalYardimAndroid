1cc00910e0e09ca96ea00b32cafd1e956e56e775
Muhammed Almaz <14542016@firat.edu.tr>
2018-12-20 10:26:04 +0300

    v0.4
    Bağışçı İşlemleri

---
+++ app/src/main/java/com/muhammedalmaz/sosyalyardim/fragmentler/IhtiyacSahibiEkle.java
@@ -0,0 +1,30 @@
+package com.muhammedalmaz.sosyalyardim.fragmentler;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.muhammedalmaz.sosyalyardim.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IhtiyacSahibiEkle extends Fragment {
+
+
+    public IhtiyacSahibiEkle() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ihtiyac_sahibi_ekle, container, false);
+    }
+
+}
